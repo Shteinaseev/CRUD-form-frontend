8109570e3072c902bfe828b7243d0dda82d1ebf0
Elitna skola 18 <shteinaseev@gmail.com>
2026-03-24 18:05:09 +0100

    added rendering entity-cards from db

--- src/components/entity-card.js
+++ src/components/entity-card.js
@@ -1,6 +1,37 @@
 import styles from './entity-card.css?inline';
 
 export class entityCard extends HTMLElement {
-    static observedAttributes = ['title', 'if', 'min-length', 'max-length', 'icon'];
+    // static observedAttributes = ['title', 'min-length', 'max-length', 'icon'];
 
+    liElementsList = []
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        console.log(this.liElementsList)
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+                ${styles}
+            </style>
+            <ul>
+               ${this.liElementsList.join("")}
+            </ul>
+        `
+    }
+
+    renderLiEls() {
+        for (let attr of this.attributes) {
+            console.log(`<li>${attr.name}: ${attr.value}</li>`);
+            this.liElementsList.push(`<li>${attr.name}: ${attr.value}</li>`);
+        }
+    }
+
+    connectedCallback() {
+        this.renderLiEls();
+        this.render();
+    }
 }
+
+customElements.define("entity-card", entityCard); 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,37 @@
 import { formGroup } from "../components/form-group";
 import { dotsGenerator } from "./dots";
+import { entityCard } from "../components/entity-card";
 
 class CRUD {
-    constructor(){
+    selectors = {
+        root: '[data-js]',
+        container: '[data-js-entity-card-cont]'
+    }
+
+    constructor() {
+        this.root = document.querySelector(this.selectors.root);
+        this.container = this.root.querySelector(this.selectors.container)
+
+        console.log(this.container)
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => data.forEach((el, i) => {
+                if (i <= 13) {
+                    this.container.append(this.createEntityCardEl(el));
+                }
+            }))
+    }
+
+    createEntityCardEl(data) {
+        const card = document.createElement('entity-card');
+        this.updateAttrs(card, data);
+        return card;
+    }
+
+    updateAttrs(card, data) {
+        for (const [key, value] of Object.entries(data)) {
+            card.setAttribute(key, value);
+        }
     }
 }
 
