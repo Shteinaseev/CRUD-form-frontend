b99ec2b08d8574c7d12bdb336f956cbf56edc656
Elitna skola 18 <shteinaseev@gmail.com>
2026-03-30 12:57:09 +0200

    some fixses

--- src/components/form-group.js
+++ src/components/form-group.js
@@ -3,15 +3,12 @@ import * as icons from '@boxicons/js';
 
 export class formGroup extends HTMLElement {
 
-    static observedAttributes = ['title', 'type', 'min-length', 'max-length', 'icon'];
+    static get observedAttributes() {
+        return ['title', 'type', 'icon', 'min-length', 'max-length', 'number'];
+    }
 
-    title = this.getAttribute('title');
-    type = this.getAttribute('type');
-    minLength = Number(this.getAttribute('min-length')) || 3;
-    maxLength = Number(this.getAttribute('max-length')) || 15;
-    id = this.title.toLowerCase();
-    iconsAttr = this.getAttribute('icon').toString();
-    icon = icons.createSvgString(icons[this.iconsAttr]);
+    minLength = 0;
+    maxLength = 15;
 
     constructor() {
         super();
@@ -21,26 +18,82 @@ export class formGroup extends HTMLElement {
     attributeChangedCallback(name, oldValue, newValue) {
         if (oldValue === newValue) return;
         if (name === 'title') this.title = newValue;
-        if (name === 'type') this.type = Number(newValue);
-        if (name === 'min-length') this.minlength = Number(newValue) || 3;
-        if (name === 'max-length') this.maxlength = Number(newValue) || 15;
+        if (name === 'type') this.type = newValue;
+        if (name === 'min-length') this.minLength = newValue ? newValue : 0;
+        if (name === 'max-length') this.maxLength = newValue ? newValue : 15;
         if (name === 'icon') this.iconsAttr = newValue.toString();
+        if (name === 'number') this.isNumber = newValue === 'true' ? true : false;
+
+        if (this.iconsAttr) {
+            this.icon = icons.createSvgString(icons[this.iconsAttr]);
+        } else {
+            this.icon = '';
+        }
+
+        if (this.type === 'text') {
+            this.attrsString = `minlength="${this.minLength}" maxlength="${this.maxLength}"
+                ${this.isNumber ? 'pattern="\d*"' : ''}
+            `;
+        } else if (this.type === 'date') {
+            this.attrsString = `max="${new Date().getFullYear()}-12-31"`;
+        } else {
+            this.attrsString = '';
+        }
 
         this.render();
+        const input = this.shadowRoot.querySelector('.field-control');
+        if (input) {
+            input.addEventListener('input', () => {
+                if (this.isNumber) {
+                    input.value = input.value.replace(/\D/g, '');
+                }
+            });
+        }
+    }
+
+    generateValuesList() {
+        this.valuesList = [];
+        for (const attr of this.attributes) {
+            if (attr.name.startsWith('value')) {
+                this.valuesList.push(attr.value);
+            }
+        }
+    }
+
+    generateBlockEl() {
+        const block = document.createElement('div');
+        this.generateValuesList();
+
+        if (this.type === 'radio') {
+            this.valuesList.forEach((value, i) => {
+                block.classList.add('radio-group');
+                block.innerHTML += `
+                    <div class="radio-block">
+                        <input id="${this.id}-${i}" name="${this.title}" class="field-control" type="radio" value="${value}">
+                        <label for="${this.id}-${i}">${value}</label>
+                    </div>
+                `;
+            });
+        } else {
+            block.classList.add('block');
+            block.innerHTML = `
+                <input id="${this.id}" name="${this.title}" class="field-control" type="${this.type}" 
+                ${this.attrsString}>
+                ${this.icon}    
+            `;
+        }
+
+        return block;
     }
 
     render() {
+        const block = this.generateBlockEl();
         this.shadowRoot.innerHTML = `
             <style>
                 ${styles}
             </style>
             <label for="${this.id}">${this.title}</label>
-            <div class="block">
-                <input id="${this.id}" name="${this.title}" class="field-control" type="${this.type}" 
-                minlength="${this.minLength}" maxlength="${this.maxLength}"
-                    required aria-errormessage="${this.id}-errors">
-                ${this.icon}    
-            </div>
+            ${block.outerHTML}
             <span class="field-errors" id="${this.id}-errors" data-js-field-errors></span>
         `
     }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,35 +14,6 @@ class CRUD {
         arrow: '[data-js-arrow]'
     }
 
-    dragStart = (e) => {
-        this.isDragging = true;
-        this.container.classList.add("dragging");
-        this.startX = e.pageX;
-        this.startScrollLeft = this.container.scrollLeft;
-    }
-
-    dragging = (e) => {
-        if (!this.isDragging) return;
-        this.container.scrollLeft = this.startScrollLeft - (e.pageX - this.startX);
-    }
-
-    dragStop = () => {
-        this.isDragging = false;
-        this.container.classList.remove("dragging");
-    }
-
-    infiniteScroll = () => {
-        if (this.container.scrollLeft === 0) {
-            this.container.classList.add("no-transition");
-            this.container.scrollLeft = this.container.scrollWidth - (2 * this.container.offsetWidth);
-            this.container.classList.remove("no-transition");
-        } else if (Math.ceil(this.container.scrollLeft) >= this.container.scrollWidth - this.container.offsetWidth) {
-            this.container.classList.add("no-transition");
-            this.container.scrollLeft = this.container.offsetWidth;
-            this.container.classList.remove("no-transition");
-        }
-    }
-
     constructor() {
         getIcons({
             icons: { ChevronLeft, ChevronRight, Alarm }
@@ -53,23 +24,15 @@ class CRUD {
         this.arrowsBtns = document.querySelectorAll('.arrow');
         this.cardPervView = Math.round(this.container.offsetWidth / this.firstCardWidth);
 
-        fetch('https://mocki.io/v1/8b08d074-93c2-42be-bb3c-94d1248c9925')
+        fetch('https://mocki.io/v1/840e113d-8cfc-4286-85d5-9742b876cb08')
             .then(response => response.json())
             .then(data => {
+                data = data.slice(0, 2);
                 data.forEach((el, i) => {
                     this.container.append(this.createEntityCardEl(el));
                 })
-                const containerChildrens = [...this.container.children];
-                containerChildrens.slice(-this.cardPerView).reverse().forEach(card => {
-                    console.log("sdds")
-                    this.container.insertAdjacentHTML("afterbegin", card.outerHTML);
-                })
-
-                containerChildrens.slice(0, this.cardPerView).forEach(card => {
-                    this.container.insertAdjacentHTML("beforeend", card.outerHTML);
-                })
             })
-        this.bindEvents();
+        // this.bindEvents();
     }
 
     createEntityCardEl(data) {
@@ -85,15 +48,6 @@ class CRUD {
     }
 
     bindEvents() {
-        this.container.addEventListener("mousedown", this.dragStart);
-        this.container.addEventListener("mousemove", this.dragging, { capture: true });
-        document.addEventListener("mouseup", this.dragStop);
-        this.arrowsBtns.forEach(btn => {
-            btn.addEventListener("click", () => {
-                this.container.scrollLeft += btn.id === "left" ? -this.firstCardWidth : this.firstCardWidth
-            })
-        })
-        this.container.addEventListener("scroll", this.infiniteScroll);
     }
 }
 
