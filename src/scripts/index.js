de93896c49ee0332984cf99dbc32d01f6db0f65f
Elitna skola 18 <shteinaseev@gmail.com>
2026-04-29 17:16:23 +0200

    added from-groups changing

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,6 +7,7 @@ import { getIcons, Home, ChevronLeft, ChevronRight, Menu, Plus, InfoCircle, Help
 
 class CRUD {
     isDragging = false;
+    index = 2;
     startX = 0;
     firstCardWidth = 350;
     selectors = {
@@ -17,7 +18,32 @@ class CRUD {
         postForm: '[data-js-post-form]',
         navbar: '[data-js-navbar]',
         navbarBtn: '[data-js-navbar-btn]',
+        homeBtn: '[data-js-navbar-home-btn]',
+        addBtn: '[data-js-navbar-add-btn]',
+        infoBtn: '[data-js-navbar-info-btn]',
+        postFormContainer: '[data-js-post-form-container]',
+        btnWrapper: '[data-js-btn-wrapper]'
     }
+
+    firstFormFields = [
+        { label: "Ime", name: "first_name", type: "text", icon: "User" },
+        { label: "Prezime", name: "last_name", type: "text", icon: "User" },
+        { label: "Datum rodjenja", name: "birthday", type: "date", icon: "" },
+        { label: "JMBG", type: "text", icon: "User", minLength: 13, maxLength: 13, number: true },
+        { label: "Broj Stana", type: "number", icon: "Home" },
+        { label: "Telefon fiksni", type: "tel", icon: "Phone" },
+        { label: "Telefon mobilni", type: "tel", icon: "Phone" },
+        { label: "Nova lozinka", type: "text", icon: "Lock" },
+        { label: "Potvrda nove lozinke", type: "text", icon: "Lock" },
+        { label: "Pol", name: "gender", type: "radio", icon: "User", options: ["Male", "Female", "Other"] },
+        { label: "Datum prvog upisa", type: "date", icon: "" },
+        { label: "Email", type: "email", icon: "Envelope" },
+        { label: "Username", type: "text", icon: "User" }
+    ];
+
+    secondFormFields = [
+        { label: "Naziv smera", name: "direction_name", type: "text", icon: "User" },
+    ]
 
     constructor() {
         getIcons({
@@ -26,12 +52,16 @@ class CRUD {
 
         this.root = document.querySelector(this.selectors.root);
         this.navbar = document.querySelector(this.selectors.navbar);
+        this.homeBtn = this.navbar.querySelector(this.selectors.homeBtn)
+        this.addBtn = this.navbar.querySelector(this.selectors.addBtn);
         this.navbarBtn = this.navbar.querySelector(this.selectors.navbarBtn);
         this.section = this.root.querySelector(this.selectors.section);
         this.container = this.section.querySelector('.container');
         this.btnShowAll = this.root.querySelector(this.selectors.btnShowAll);
         this.postForm = this.root.querySelector(this.selectors.postForm);
-
+        this.postFormContainer = this.postForm.querySelector(this.selectors.postFormContainer);
+        this.btnWrapper = this.postFormContainer.querySelector(this.selectors.btnWrapper);
+        this.renderFormGroups();
         fetch('https://mocki.io/v1/840e113d-8cfc-4286-85d5-9742b876cb08')
             .then(response => response.json())
             .then(data => {
@@ -39,6 +69,36 @@ class CRUD {
                 this.renderEntityCards();
             })
         this.bindEvents();
+    }
+
+    createFormGroup() {
+        const formGroup = document.createElement('form-group');
+        return formGroup;
+    }
+
+    updateAttrs(el, obj) {
+        for (const [key, value] of Object.entries(obj)) {
+            el.setAttribute(key, value);
+        }
+    }
+
+    renderFormGroups() {
+        switch (this.index) {
+            case 1:
+                this.firstFormFields.forEach((obj) => {
+                    const el = this.createFormGroup();
+                    this.updateAttrs(el, obj);
+                    this.postFormContainer.prepend(el);
+                })
+                break;
+            case 2:
+                this.secondFormFields.forEach((obj) => {
+                    const el = this.createFormGroup();
+                    this.updateAttrs(el, obj);
+                    this.postFormContainer.prepend(el);
+                })
+                break;
+        }
     }
 
     renderEntityCards() {
@@ -71,6 +131,7 @@ class CRUD {
             j += 5;
         });
     }
+
 
     renderHeaderGridItem() {
         const el = this.createHeaderGridItem();
@@ -106,12 +167,6 @@ class CRUD {
             header.innerHTML += `<p>${key}</p>`;
         }
         return header;
-    }
-
-    updateAttrs(card, data) {
-        for (const [key, value] of Object.entries(data)) {
-            card.setAttribute(key, value);
-        }
     }
 
     #fadeOut(el, timeout = 500) {
@@ -173,14 +228,39 @@ class CRUD {
             if (!isClickInsideNavbar && !this.navbar.classList.contains('scrolled')) {
                 this.navbar.classList.add('scrolled');
             }
+
+            if (isClickInsideNavbar) {
+                if (e.target === this.addBtn || e.target.closest(this.selectors.addBtn)) {
+                    this.postForm.classList.toggle('active');
+                } else if (e.target === this.homeBtn || e.target.closest(this.selectors.homeBtn)) {
+                    if (isShowingAll) {
+                        this.btnShowAll.textContent = 'Prikaži sve';
+                        this.#hideAllItems(this.section, 1000)
+                            .then(() => {
+                                this.renderEntityCards();
+                                isShowingAll = false;
+                                this.btnShowAll.disabled = false;
+                                this.btnShowAll.classList.remove('disactivated')
+                            })
+                    }
+                }
+            }
+        })
+
+        this.btnWrapper.addEventListener('click', (e) => {
+            const btnIndex = parseInt(e.target.getAttribute('data-js-btn-index'));
+            this.index = btnIndex;
+            this.renderFormGroups();
         })
 
         this.navbarBtn.addEventListener('click', () => {
             this.navbar.classList.toggle('scrolled');
         })
 
+
         this.btnShowAll.addEventListener('click', () => {
             if (!isShowingAll) {
+                this.btnShowAll.classList.add('disactivated')
                 this.btnShowAll.textContent = 'Sakrij sve';
                 this.btnShowAll.disabled = true;
                 this.#hideAllItems(this.container, 1000)
@@ -189,16 +269,17 @@ class CRUD {
                         this.renderGridItems();
                         this.renderHeaderGridItem();
                         isShowingAll = true;
-                        this.btnShowAll.disabled = false;
                     })
             } else if (isShowingAll) {
                 this.btnShowAll.textContent = 'Prikaži sve';
-                this.btnShowAll.disabled = true;
+                this.btnShowAll.classList.remove('disactivated')
+
+                this.btnShowAll.disabled = false;
                 this.#hideAllItems(this.section, 1000)
                     .then(() => {
                         this.renderEntityCards();
-                        isShowingAll = false;
                         this.btnShowAll.disabled = false;
+                        this.btnShowAll.classList.remove('disactivated')
                     })
             }
         });
